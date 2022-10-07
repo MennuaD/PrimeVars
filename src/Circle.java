a964df7d8d0126d562bb01afdaa7eb28658ef69e
student <student@email.com>
2022-10-07 12:11:36 -0400

    tarhs

---
+++ src/Circle.java
@@ -0,0 +1,42 @@
+public class Circle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        /* Returns the area of the circle with given radius
+           Area of a circle = pi * r ^ 2
+         */
+        public double area() {
+             double x;
+           x = (Math.pow(radius, 2)) * Math.PI ;
+
+            return x;
+        }
+
+        /* Returns the circumference of the circle with given radius
+           Circumference of a circle = pi * d
+           (d = diameter = 2 * r)
+         */
+      //. public double circumference()
+       // { /* implement this method! */ }
+
+        /* Updates the radius of the circle to newRadius
+         */
+        public void setRadius(double newRadius)
+        {
+            radius = newRadius;
+        }
+
+        /* Returns a String that includes info about the circle,
+           including its radius, area, and circumference on separate lines;
+           see example output below
+         */
+     //   public String toString()
+       // { /* implement this method! */ }
+    }
+
+
